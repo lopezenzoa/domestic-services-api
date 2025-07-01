1c1a59a2489d244404d4e440b100f2f872437f8f
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-07-01 19:38:52 -0300

    initial commit

---
+++ src/main/java/com/portfolio/domestic_services/DomesticServicesApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.domestic_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesticServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesticServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/domestic_services/DomesticServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.domestic_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
