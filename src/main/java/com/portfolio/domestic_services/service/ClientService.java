c6129c7c1267f4f1ac085de7c628b6927c24634b
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-07-14 10:49:07 -0300

    removes buildProvider() method of client service

--- src/main/java/com/portfolio/domestic_services/service/ClientService.java
+++ src/main/java/com/portfolio/domestic_services/service/ClientService.java
@@ -1,14 +1,11 @@
 package com.portfolio.domestic_services.service;
 
-import com.portfolio.domestic_services.dto.RegisterDataDTO;
 import com.portfolio.domestic_services.dto.ClientDTO;
-import com.portfolio.domestic_services.model.Provider;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ClientService {
-    Provider buildProvider(RegisterDataDTO dto);
     Optional<ClientDTO> create(ClientDTO dto);
     Optional<ClientDTO> update(ClientDTO newDto);
     Optional<ClientDTO> getById(Long id);
--- src/main/java/com/portfolio/domestic_services/service/impl/ClientServiceImpl.java
+++ src/main/java/com/portfolio/domestic_services/service/impl/ClientServiceImpl.java
@@ -1,10 +1,7 @@
 package com.portfolio.domestic_services.service.impl;
 
-import com.portfolio.domestic_services.dto.RegisterDataDTO;
 import com.portfolio.domestic_services.dto.ClientDTO;
 import com.portfolio.domestic_services.mappers.ClientMapper;
-import com.portfolio.domestic_services.model.Provider;
-import com.portfolio.domestic_services.model.Roles;
 import com.portfolio.domestic_services.model.Client;
 import com.portfolio.domestic_services.repository.ClientRepository;
 import com.portfolio.domestic_services.service.ClientService;
@@ -18,25 +15,6 @@ import java.util.Optional;
 public class ClientServiceImpl implements ClientService {
     @Autowired private ClientRepository repo;
     @Autowired private ClientMapper mapper;
-
-    @Override
-    public Provider buildProvider(RegisterDataDTO dto) {
-        Provider provider = new Provider();
-
-        provider.setFirstName(dto.getFirstName());
-        provider.setLastName(dto.getLastName());
-        provider.setAddress(dto.getAddress());
-        provider.setPhoneNumber(dto.getPhoneNumber());
-        provider.setEmail(dto.getEmail());
-        provider.setLicenseNumber(dto.getLicenseNumber());
-
-        // Setting up credentials
-        provider.setRole(Roles.PROVIDER);
-        provider.setUsername(dto.getUsername());
-        provider.setPassword(dto.getPassword());
-
-        return provider;
-    }
 
     @Override
     public Optional<ClientDTO> create(ClientDTO dto) {
